5eceadb8b78e7f539f011290fddfc38bce7b9746
Michael Straßburger <codepoet@cpan.org>
2017-09-03 18:52:07 +0200

    :sparkles: patchinging queryRenderedFeatures into Android bridge

--- android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLView.java
+++ android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLView.java
@@ -33,6 +33,7 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.UiSettings;
+import com.mapbox.services.commons.geojson.Feature;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -783,6 +784,16 @@ public class ReactNativeMapboxGLView extends RelativeLayout implements
     public void deselectAnnotation() {
         if (_map == null) { return; }
         _map.deselectMarkers();
+    }
+
+    public java.util.List<Feature> queryRenderedFeatures(android.graphics.PointF point, java.lang.String... layerIds) {
+        if (_map == null) { return new ArrayList(); }
+        return _map.queryRenderedFeatures(point, layerIds);
+    }
+
+    public java.util.List<Feature> queryRenderedFeatures(android.graphics.RectF rect, java.lang.String... layerIds) {
+        if (_map == null) { return new ArrayList(); }
+        return _map.queryRenderedFeatures(rect, layerIds);
     }
 
     // Custom Marker View Adapter - Adapts a MarkerView to display an custom react native view.
